8b2925e00b4827965d33c42b61ccda0fb3f762fd
Frederik Winther <frederikwinther@msn.com>
2017-09-29 10:37:57 +0200

    Refactoring code

--- Index.js
+++ Index.js
@@ -110,7 +110,7 @@ module.exports = {
 function addNodeTo(currentBucket, nodeID, Port) {
     var tempNode;
     if (currentBucket.length >= constants.k) {
-        var deadNode = pingAllIdsInBucket();
+        var deadNode = pingAllIdsInBucket(currentBucket);
 
         // If a pinged node doesn't respond, this node will be removed.
         if (deadNode !== null) {
@@ -149,45 +149,40 @@ function isBucketFull(currentBucket){
 /**
  This method returns the first dead node it finds
  */
-/*
 function pingAllIdsInBucket(currentBucket) {
     if (nodeList.length > 0){
         var counter = 0;
         var foundDeadNode = false;
         var deadNode;
-        var currentNode = nodeList[counter];
-        var url = constants.ipAddress + currentNode.port;
-        console.log("URL", url);
+        var currentNode;
+        var url;
+
         //This function should ping all the IDs in the list until it finds a dead node
-        if(counter < nodeList.length && foundDeadNode == false) {   //Skal det ikke være en While løkke eller en for-løkke?
-            //The format is https://ipaddress/port/nodeID
+        while(counter < nodeList.length && foundDeadNode == false) {
+
+            currentNode = currentBucket[counter];
+            url = constants.ipAddress + currentNode.port; //The format is https://ipaddress/port
+
+
+            // Connects to the defined url and checks if it exists or is dead
             axios.get(url)
                 .then(function (response) {
                     console.log("success",response);
+                    counter++
                 })
                 .catch(function (error) {
                     console.log(error);
-                });
-            /!*
-            http.get(url , function(parameters) {
-                var res = parameters.res;
-
-                if (res == 200) {
-                    counter++
-                }
-
-                else {
                     foundDeadNode = true;
                     deadNode = nodeList[counter];
-                }
-            });
-            *!/
+                });
         }
 
         //Returns the dead node if there has been found one
         return deadNode;
     }
-};*/
+}
+
+
 
 //---------------------------------------- ROUTING TABLE FUNCTIONS -------------------------------------\\
 
